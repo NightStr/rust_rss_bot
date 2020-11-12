37455a15e476c5c6d36603ffceebe9bd7cd3af96
Смагин Павел <p.smagin@napoleonit.ru>
2020-11-12 18:30:21 +0500

    base echo bot

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,19 @@
 pub mod rss {
     use rss;
     use chrono::{DateTime, Utc};
-    use telegram_bot;
+    use telegram_bot::*;
+    use futures::StreamExt;
+    use String;
 
     pub type Item = rss::Item;
 
-    pub struct RssItemsGetter{
+    pub struct RssItemsGetter {
         channel: rss::Channel
     }
 
     impl RssItemsGetter {
         pub fn new(url: &str) -> RssItemsGetter {
-            RssItemsGetter{ channel: rss::Channel::from_url(url).unwrap() }
+            RssItemsGetter { channel: rss::Channel::from_url(url).unwrap() }
         }
         pub fn get_rss_items(&self, filtered_date: DateTime<Utc>) -> impl Iterator<Item=&rss::Item> {
             self.channel.items().iter().filter(move |i| {
@@ -27,6 +29,33 @@ pub mod rss {
     }
 
     pub struct TelegramBot {
-        api: telegram_bot::Api
+        api: Api,
+    }
+
+    impl TelegramBot {
+        pub fn new<T: Into<String> >(token: T) -> Self
+        {
+            return TelegramBot {
+                api: Api::new(token.into()),
+            }
+        }
+
+        pub async fn run(self) -> Result<(), &'static str> {
+            let mut stream = self.api.stream();
+            while let Some(update) = stream.next().await {
+                // If the received update contains a new message...
+                let update = update.expect("Failed to update");
+                if let UpdateKind::Message(message) = update.kind {
+                    if let MessageKind::Text { ref data, .. } = message.kind {
+                        println!("<{}>: {}", &message.from.first_name, data);
+                        self.api.send(message.text_reply(format!(
+                            "Hi, {}! You just wrote '{}'",
+                            &message.from.first_name, data
+                        ))).await;
+                    }
+                }
+            };
+            Ok(())
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
+use std::env;
+
 use chrono::prelude::*;
 use chrono::Duration;
+use tokio::prelude::*;
 
-use feed_bot::rss::{ RssItemsGetter, Item };
+use feed_bot::rss::{ RssItemsGetter, TelegramBot };
 
-fn main() {
-    let v: Vec<Item> = Vec::new();
-    let item_getter = RssItemsGetter::new("https://readrust.net/all/feed.rss");
-    for item in item_getter.get_rss_items(Utc::now() - Duration::days(10)) {
-        println!("{}", item.pub_date().unwrap())
-    }
+
+#[tokio::main]
+async fn main() {
+    let telegram_bot = TelegramBot::new(
+        env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set")
+    );
+    telegram_bot.run().await.unwrap();
 }
